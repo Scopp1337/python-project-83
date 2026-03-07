c4fa84331ca411ebb35d21f854b00e850b6c5fc6
Roman <roma.mazin.47@gmail.com>
2026-03-07 11:21:04 +0300

    Fix db

--- database.sql
+++ database.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS url_checks (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         url_id INT REFERENCES urls (id) NOT NULL,
         status_code INT,
-        h1 VARCHAR(255),
-        title VARCHAR(255),
-        description VARCHAR(255),
+        h1 TEXT,
+        title TEXT,
+        description TEXT,
         created_at DATE DEFAULT CURRENT_TIMESTAMP
 );
